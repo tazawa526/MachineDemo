57fa9f24de3f899fc9db77cf72be0c894a12aec2
Administrator <tianze05@163.com>
2020-03-20 00:12:19 +0800

    2020.03.20 第一次更新

--- chapter1/src/SklearnDemo/sklearn_006.py
+++ chapter1/src/SklearnDemo/sklearn_006.py
@@ -7,7 +7,7 @@
 
 @Modify Time      @Author    @Version    @Desciption
 ------------      -------    --------    -----------
-2020/3/19 22:27   TAZAWA      1.0         None
+2020/3/19 22:27   TAZAWA      1.0         交叉
 '''
 
 # import lib
